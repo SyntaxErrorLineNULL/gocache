48409bfa4af38b6d91e6405eae3920a1845a0600
syntaxErrorLineNil <cyberorange16@gmail.com>
2024-08-23 16:24:45 +0300

    default ttl, and memory struct fields

--- cache/memory_cache.go
+++ cache/memory_cache.go
@@ -1,5 +1,32 @@
 package cache
 
+import (
+	"container/list"
+	"context"
+	"golang.org/x/sync/singleflight"
+	"sync"
+	"time"
+)
+
+// DefaultTTL is the default time-to-live duration for cache items.
+const DefaultTTL = 1 * time.Hour
+
 // MemoryCache represents an in-memory cache with TTL support.
 type MemoryCache[K comparable, V any] struct {
+	// parentCtx is the parent context used to manage the cache's goroutines.
+	parentCtx context.Context
+	// List to maintain the order of items.
+	list *list.List
+	// Map for quick lookups.
+	items map[K]*list.Element
+	// to search for expired data, we use the map. To reduce the impact on the data warehouse
+	expirationBuckets map[K]*list.Element
+	// wg is a WaitGroup for managing goroutines.
+	wg sync.WaitGroup
+	// ttl represents the default time-to-live duration for cache items.
+	ttl time.Duration
+	// Mutex for thread safety.
+	mutex sync.RWMutex
+	// New field for singleflight.Group
+	group singleflight.Group
 }
